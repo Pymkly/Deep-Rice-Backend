87a47193924a49c327c3b37571a4fa4f1b3bfaf0
Pymkly <hrivonandrasana@gmail.com>
2025-04-04 09:12:10 +0200

    next update

---
+++ base/postgresql/data/000060-potos/deeprice-data-2025-04-04-001.sql
@@ -0,0 +1,9 @@
+INSERT INTO potos (id, parcel_id, title, global_location, created_at, ref)
+VALUES (
+  2,
+  1,
+  'Poto 1',
+  '0101000020E61000001AE81609CF0533C084C51DBE2EC44740',
+  '2025-03-25 11:42:30.423594',
+  'POTO#0001'
+);
